3d863997a60cc674902dc884a2e4c6384256a0e9
Roni Athayde <pontes014@gmail.com>
2023-07-31 21:04:17 -0300

    feat: initial commit

---
+++ core/crud.ts
@@ -0,0 +1,123 @@
+// const fs = require("fs")
+import fs from "fs";
+import { v4 as uuid } from "uuid";
+
+const DB_FILE_PATH = "./core/db";
+
+// console.log("[CRUD]");
+
+type UUID = string;
+
+interface Todo {
+  id: UUID;
+  date: string;
+  content: string;
+  done: boolean;
+}
+
+function create(content: string): Todo {
+  const todo: Todo = {
+    id: uuid(),
+    date: new Date().toISOString(),
+    content: content,
+    done: false,
+  };
+
+  const todos: Array<Todo> = [...read(), todo];
+
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        todos,
+        dogs: [],
+      },
+      null,
+      2
+    )
+  );
+  return todo;
+}
+
+export function read(): Array<Todo> {
+  const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+  const db = JSON.parse(dbString || "{}");
+
+  if (!db.todos) {
+    return [];
+  }
+
+  return db.todos;
+}
+
+function update(id: UUID, partialTodo: Partial<Todo>): Todo {
+  let updatedTodo;
+  const todos = read();
+  todos.forEach((currentTodo) => {
+    const isToUpdate = currentTodo.id === id;
+
+    if (isToUpdate) {
+      updatedTodo = Object.assign(currentTodo, partialTodo);
+    }
+  });
+
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        todos,
+      },
+      null,
+      2
+    )
+  );
+  if (!updatedTodo) {
+    throw new Error("Please, provide another ID!");
+  }
+
+  return updatedTodo;
+}
+
+function updateContentById(id: UUID, content: string): Todo {
+  return update(id, {
+    content,
+  });
+}
+
+function deleteById(id: UUID) {
+  const todos = read();
+
+  // const termToFilterNormalized = "A".toLocaleLowerCase()
+
+  const todosWithoutOne = todos.filter((todo) => {
+    if (id === todo.id) {
+      return false;
+    }
+    return true;
+  });
+
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        todos: todosWithoutOne,
+      },
+      null,
+      2
+    )
+  );
+}
+
+function CLEAR_DB() {
+  fs.writeFileSync(DB_FILE_PATH, "");
+}
+// CLEAR_DB();
+// const secondTodo = create("Segunda TODO");
+// const extraTodo = create("EXTRA TODO");
+// deleteById(secondTodo.id);
+// const thirdTodo = create("Terceira TODO");
+// // update(secondTodo.id, {
+// //   content: "Mais uma atualização"
+// // })
+// updateContentById(thirdTodo.id, "Atualizada!");
+// console.log(read());
---
+++ pages/api/todos.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { read } from "@db-crud-todo";
+import { todoController } from "@server/controller/todo";
+
+export default function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  // eslint-disable-next-line no-console
+  console.log(request.method);
+
+  if (request.method === "GET") {
+    todoController.get(request, response);
+
+    return;
+  }
+
+  response.status(405).json({
+    message: "Method not allowed",
+  });
+}
